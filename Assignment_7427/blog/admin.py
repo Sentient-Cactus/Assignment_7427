2a23c1bf6363cc1b9094d4a92fd19c1fd4d9e438
Kyung-Min <kyungminkimnz@gmail.com>
2021-11-03 08:48:18 +1300

    Revert "Making copy"
    This reverts commit 1f2605abd8ba645c1dbb0179be842877e79a4b2e.

--- Assignment_7427/blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from blog.models import Dog, Activity, Breed, Comment
-
-admin.site.register(Dog)
-admin.site.register(Activity)
-admin.site.register(Breed)
-admin.site.register(Comment)
